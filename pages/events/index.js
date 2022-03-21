47425a7c3656b73919837698b60617a91e90c05c
Bovornpot Jindachuen <bovornpot.ji@glsict.com>
2022-03-21 11:27:24 +0700

    added filtered events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,26 @@
-function AllEventsPagePage() {
-    return (
-      <div>
-        <h1>All Events</h1>
-      </div>
-    );
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+import EventSearch from "../../components/events/events-search";
+
+function AllEventsPage() {
+  const router = useRouter();
+  const events = getAllEvents();
+
+  function findEventHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
   }
-  
-  export default AllEventsPagePage;
-  
+
+  return (
+    <Fragment>
+      <EventSearch onSearch={findEventHandler} />
+      <EventList items={events} />
+    </Fragment>
+  );
+}
+
+export default AllEventsPage;
